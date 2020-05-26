400d27743cf9ea0d19592298de091e0788dc9ef9
Brandon <Blabrake1@gmail.com>
2020-05-26 19:22:16 -0400

    Simple AI done

---
+++ Assets/Scripts/EnemyTerritory.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTerritory : MonoBehaviour
+{
+    GameObject jeff, handman, shooter;
+    bool interritory = false;
+    public BoxCollider territory;
+
+    public GameObject enemy;
+    Enemy_AI basicenemy;
+
+    GameObject playerinzone;
+
+    void Start()
+    {
+        enemy = this.gameObject;
+        jeff = GameObject.Find("Jeff");
+        handman = GameObject.Find("HandMan");
+        shooter = GameObject.Find("Shooter");
+        basicenemy = enemy.GetComponent<Enemy_AI>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interritory == true)
+        {
+            basicenemy.MoveToPlayer(playerinzone.transform);
+        }
+
+        if (interritory == false)
+        {
+            basicenemy.wonder();
+        }
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (jeff.GetComponent<Player>().activeplayer == true && col.gameObject == jeff)
+        {
+            playerinzone = jeff;
+            interritory = true;
+        }
+        
+        if(handman.GetComponent<Player>().activeplayer == true && col.gameObject == handman)
+        {
+            playerinzone = handman;
+            interritory = true;
+        }
+
+        if (shooter.GetComponent<Player>().activeplayer == true && col.gameObject == shooter)
+        {
+            playerinzone = shooter;
+            interritory = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (jeff.GetComponent<Player>().activeplayer == true && col.gameObject == jeff)
+        {
+            interritory = false;
+        }
+        if (handman.GetComponent<Player>().activeplayer == true && col.gameObject == handman)
+        {
+            interritory = false;
+        }
+        if (shooter.GetComponent<Player>().activeplayer == true && col.gameObject == shooter)
+        {
+            interritory = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour
+{
+    public float speed = 2f;
+    public float attackRange = 1f;
+    public int attackDamage = 1;
+    public float timeBetweenAttacks;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveToPlayer(Transform player)
+    {
+        transform.LookAt(player.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+        //move towards player
+        if (Vector3.Distance(transform.position, player.position) > attackRange)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+
+    public void wonder()
+    {
+        //for later
+    }
+
+}
