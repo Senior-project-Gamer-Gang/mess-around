058158c8eedb9eee86111e3fea8cf09c0b1cf3ce
Tribow <realms007@gmail.com>
2020-06-01 23:35:47 -0400

    Camera quickfix
    camera quickfix

---
+++ Assets/Scripts/CameraPivotScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPivotScript : MonoBehaviour
+{
+    private CameraScript cameraScript;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraScript = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraScript>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.localPosition = new Vector3(0f, 1f, -cameraScript.zoomDistance);
+    }
+}
--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -104,9 +104,13 @@ public class CameraScript : MonoBehaviour
             //this is unless the player is too far, then it should change its max speed to its own max speed rather than the player's.
             //reentering the zone makes it match the player's speed again.
             //so I need to figure out how far the camera can be from the player without choosing its own max speed
-            maxSpeed = player.GetComponent<Player>().speed; //max speed should match the player's speed every time isMoving becomes true
+            maxSpeed = player.GetComponent<Player>().speed / 94; //max speed should match the player's speed every time isMoving becomes true
 
             cameraSpeed += Time.deltaTime * cameraAcceleration;
+            if (cameraSpeed > maxSpeed)
+            {
+                cameraSpeed = maxSpeed;
+            }
             if(distanceBetweenCameraAndPlayer > tooFarDistance || maxSpeed > maxFollowSpeed)
             {
                 maxSpeed = maxFollowSpeed;
