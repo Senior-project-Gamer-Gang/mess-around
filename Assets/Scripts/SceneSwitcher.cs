b6a9733062d13328362cbec55e73a0ec3784096a
Brandon <Blabrake1@gmail.com>
2020-06-15 23:07:46 -0400

    whr

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
-    
-    void Start()
+    void OnTriggerEnter(Collider Col)
     {
-        
+        if(Col.gameObject.tag == "Player" && this.gameObject.name == "Level1_Trig")
+        {
+            Level1Switch();
+        }
+        if (Col.gameObject.tag == "Player" && this.gameObject.name == "Level2_Trig")
+        {
+            Level2Switch();
+        }
+        if (Col.gameObject.tag == "Player" && this.gameObject.name == "Level3_Trig")
+        {
+            Level3Switch();
+        }
+        if (Col.gameObject.tag == "Player" && this.gameObject.name == "Hub_Trig")
+        {
+            LevelHubSwitch();
+        }
+
     }
 
-    
-    void Update()
+
+
+    public void Level1Switch()
     {
-        
+        SceneManager.LoadScene("Level1");
+    }
+    public void Level2Switch()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void Level3Switch()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+        public void LevelHubSwitch()
+    {
+        SceneManager.LoadScene("Hub");
     }
 }
+
