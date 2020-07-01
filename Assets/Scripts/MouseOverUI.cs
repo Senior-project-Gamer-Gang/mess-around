b00d37cc5fc084dd5d047417be69a4130e20f40e
Brandon <Blabrake1@gmail.com>
2020-07-01 03:58:59 -0400

    MainMenu

--- Assets/Scripts/MouseOverUI.cs
+++ Assets/Scripts/MouseOverUI.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MouseOverUI : MonoBehaviour
 {
+    public GameObject Character;
     public Sprite Starting, Ending, startingimg,Endingimg;
     public Image img;
     public void OnMouseOver()
     {
+        Character.SetActive(true);
         this.gameObject.GetComponent<Button>().image.sprite = Ending;
         img.sprite = Endingimg;
     }
     public void OnMouseExit()
     {
+        Character.SetActive(false);
         this.gameObject.GetComponent<Button>().image.sprite = Starting;
         img.sprite = startingimg;
     }
